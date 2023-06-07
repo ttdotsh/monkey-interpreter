df2b649416b623d1f9438fb7c97d6afb4a602219
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-06-07 08:56:56 -0400

    add iterator trait to lexer + use it in repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,25 +1,22 @@
 use std::io::{BufRead, Result, Write};
 
 use crate::lexer::Lexer;
-use crate::token::Token;
 
 pub fn start<R: BufRead, W: Write>(mut reader: R, mut writer: W) -> Result<()> {
     writeln!(writer, "Give the monkey some commands!")?;
 
     loop {
-        write!(writer, "{}: ", "🐒")?;
+        write!(writer, "{} -> ", "🐒")?;
         writer.flush()?;
 
         let mut buffer = String::new();
         _ = reader.read_line(&mut buffer)?;
-        let mut lexer = Lexer::new(buffer);
+        let lexer = Lexer::new(buffer);
 
-        while let Ok(token) = lexer.next_token() {
-            match token {
-                Token::Eof => break,
-                _ => writeln!(&mut writer, "{:?}", token)?,
-            }
-        }
+        lexer
+            .into_iter()
+            .filter_map(|t| t.ok())
+            .try_for_each(|t| writeln!(&mut writer, "{:?}", t))?;
         writer.flush()?;
     }
 }
