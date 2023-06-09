43e401a38bd6c1b4630b26ebcdad081f555c6cb9
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-06-09 07:41:10 -0400

    refactor ast implementation

--- src/ast.rs
+++ src/ast.rs
@@ -4,39 +4,41 @@ trait Node {
     fn token(&self) -> Token;
 }
 
-trait Expression {
-    // type Value;
-    //
-    // fn value() -> Self::Value;
-}
-
-struct Identifier(String);
-
-impl Node for Identifier {
-    fn token(&self) -> Token {
-        Token::Ident(self.0.to_owned())
-    }
-}
-
-impl Expression for Identifier {}
-
 #[allow(dead_code)]
-enum Statement {
-    Let {
-        name: Identifier,
-        value: Box<dyn Expression>,
-    },
+pub enum Statement {
+    Let(Let),
 }
 
 impl Node for Statement {
     fn token(&self) -> Token {
         match self {
-            Statement::Let { name: _, value: _ } => Token::Let,
+            Statement::Let(_) => Token::Let,
         }
     }
 }
 
 #[allow(dead_code)]
-struct Program {
+pub struct Let {
+    name: Identifier,
+    value: Box<Expression>,
+}
+
+#[allow(dead_code)]
+pub enum Expression {
+    Identifier(Identifier),
+}
+
+impl Node for Expression {
+    fn token(&self) -> Token {
+        match self {
+            Expression::Identifier(Identifier(s)) => Token::Ident(s.to_owned()),
+        }
+    }
+}
+
+pub struct Identifier(String);
+
+#[allow(dead_code)]
+pub struct Program {
     statements: Vec<Statement>,
 }
