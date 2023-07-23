92270d19784712d5340f50dee173b3208e400ad7
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-07-23 18:21:47 -0400

    replace individual methods for each operator with use of traits

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -60,162 +60,81 @@ impl Environment {
             Expr::IntLiteral(i) => Ok(Object::Integer(i)),
             Expr::BooleanLiteral(b) => Ok(Object::Boolean(b)),
 
-            Expr::Ident(s) => self.eval_ident(s),
-            Expr::If { check, block, alt } => self.eval_if_expr(*check, block, alt),
+            Expr::Ident(s) => {
+                let borrowed_env = self.store.borrow();
+                let var = borrowed_env.get(&s);
+                match var {
+                    Some(o) => Ok(o.clone()),
+                    None => Err(format!("Identifier not found: {}", &s)),
+                }
+            }
 
-            Expr::Prefix(op, right) => self.eval_prefix_expr(op, *right),
-            Expr::Infix(left, op, right) => self.eval_infix_expr(*left, op, *right),
+            Expr::If { check, block, alt } => {
+                if self.eval_expression(*check)?.is_truthy() {
+                    self.eval_ast(block)
+                } else {
+                    match alt {
+                        Some(block) => self.eval_ast(block),
+                        None => Ok(Object::Null),
+                    }
+                }
+            }
+
+            Expr::Prefix(op, right) => {
+                let operand = self.eval_expression(*right)?;
+                match op {
+                    Operator::Bang => Ok(!operand),
+                    Operator::Minus => -operand,
+                    _ => Err(format!("Unsupported operator as prefix: {}", op)),
+                }
+            }
+
+            Expr::Infix(left, op, right) => match op {
+                Operator::Plus => {
+                    let left = self.eval_expression(*left)?;
+                    let right = self.eval_expression(*right)?;
+                    left + right
+                }
+                Operator::Minus => {
+                    let left = self.eval_expression(*left)?;
+                    let right = self.eval_expression(*right)?;
+                    left - right
+                }
+                Operator::Multiplication => {
+                    let left = self.eval_expression(*left)?;
+                    let right = self.eval_expression(*right)?;
+                    left * right
+                }
+                Operator::Division => {
+                    let left = self.eval_expression(*left)?;
+                    let right = self.eval_expression(*right)?;
+                    left / right
+                }
+
+                Operator::LessThan => {
+                    let left = self.eval_expression(*left)?;
+                    let right = self.eval_expression(*right)?;
+                    Ok(Object::Boolean(left < right))
+                }
+                Operator::GreaterThan => {
+                    let left = self.eval_expression(*left)?;
+                    let right = self.eval_expression(*right)?;
+                    Ok(Object::Boolean(left > right))
+                }
+                Operator::Equals => {
+                    let left = self.eval_expression(*left)?;
+                    let right = self.eval_expression(*right)?;
+                    Ok(Object::Boolean(left == right))
+                }
+                Operator::NotEquals => {
+                    let left = self.eval_expression(*left)?;
+                    let right = self.eval_expression(*right)?;
+                    Ok(Object::Boolean(left != right))
+                }
+                invalid_op => Err(format!("Unsupported operator as infix: {}", invalid_op)),
+            },
 
             _ => Err(format!("Unsupported expression type: {}", expr)),
-        }
-    }
-
-    fn eval_ident(&self, name: String) -> Result<Object> {
-        let env = self.env.borrow();
-        let var = env.get(&name);
-        match var {
-            Some(o) => Ok(o.clone()),
-            None => Err(format!("Identifier not found: {}", &name)),
-        }
-    }
-
-    fn eval_if_expr(&self, check: Expr, block: Ast, alt: Option<Ast>) -> Result<Object> {
-        if self.eval_expression(check)?.is_truthy() {
-            Ok(self.eval_block(block)?)
-        } else if let Some(else_block) = alt {
-            Ok(self.eval_block(else_block)?)
-        } else {
-            Ok(Object::Null)
-        }
-    }
-
-    fn eval_prefix_expr(&self, op: Operator, right: Expr) -> Result<Object> {
-        let operand = self.eval_expression(right)?;
-        match op {
-            Operator::Bang => Ok(self.eval_bang_prefix(operand)),
-            Operator::Minus => Ok(self.eval_minus_prefix(operand)?),
-            _ => Err(format!("Unsupported operator as prefix: {}", op)),
-        }
-    }
-
-    fn eval_infix_expr(&self, left: Expr, op: Operator, right: Expr) -> Result<Object> {
-        match op {
-            Operator::Plus => {
-                let left = self.eval_expression(left)?;
-                let right = self.eval_expression(right)?;
-                self.eval_plus_infix(left, right)
-            }
-            Operator::Minus => {
-                let left = self.eval_expression(left)?;
-                let right = self.eval_expression(right)?;
-                self.eval_minus_infix(left, right)
-            }
-            Operator::Multiplication => {
-                let left = self.eval_expression(left)?;
-                let right = self.eval_expression(right)?;
-                self.eval_mult_infix(left, right)
-            }
-            Operator::Division => {
-                let left = self.eval_expression(left)?;
-                let right = self.eval_expression(right)?;
-                self.eval_div_infix(left, right)
-            }
-            Operator::LessThan => {
-                let left = self.eval_expression(left)?;
-                let right = self.eval_expression(right)?;
-                self.eval_less_infix(left, right)
-            }
-            Operator::GreaterThan => {
-                let left = self.eval_expression(left)?;
-                let right = self.eval_expression(right)?;
-                self.eval_greater_infix(left, right)
-            }
-            Operator::Equals => {
-                let left = self.eval_expression(left)?;
-                let right = self.eval_expression(right)?;
-                self.eval_equal_infix(left, right)
-            }
-            Operator::NotEquals => {
-                let left = self.eval_expression(left)?;
-                let right = self.eval_expression(right)?;
-                self.eval_not_equal_infix(left, right)
-            }
-            invalid_op => Err(format!("Unsupported operator as infix: {}", invalid_op)),
-        }
-    }
-
-    /*
-     * Prefix Expressions
-     */
-    fn eval_bang_prefix(&self, value: Object) -> Object {
-        Object::Boolean(!value.is_truthy())
-    }
-
-    fn eval_minus_prefix(&self, value: Object) -> Result<Object> {
-        match value {
-            Object::Integer(i) => Ok(Object::Integer(-i)),
-            _ => Err(format!("No such negative value of {}", value)),
-        }
-    }
-
-    /*
-     * Infix Expressions
-     */
-    fn eval_plus_infix(&self, left: Object, right: Object) -> Result<Object> {
-        match (left, right) {
-            (Object::Integer(l), Object::Integer(r)) => Ok(Object::Integer(l + r)),
-            (l, r) => Err(format!("Cannot add {} to {}", l, r)),
-        }
-    }
-
-    fn eval_minus_infix(&self, left: Object, right: Object) -> Result<Object> {
-        match (left, right) {
-            (Object::Integer(l), Object::Integer(r)) => Ok(Object::Integer(l - r)),
-            (l, r) => Err(format!("Cannot subtract {} from {}", l, r)),
-        }
-    }
-
-    fn eval_mult_infix(&self, left: Object, right: Object) -> Result<Object> {
-        match (left, right) {
-            (Object::Integer(l), Object::Integer(r)) => Ok(Object::Integer(l * r)),
-            (l, r) => Err(format!("Cannot multiply {} and {}", l, r)),
-        }
-    }
-
-    fn eval_div_infix(&self, left: Object, right: Object) -> Result<Object> {
-        match (left, right) {
-            (Object::Integer(l), Object::Integer(r)) => Ok(Object::Integer(l / r)),
-            (l, r) => Err(format!("Cannot divide {} and {}", l, r)),
-        }
-    }
-
-    fn eval_less_infix(&self, left: Object, right: Object) -> Result<Object> {
-        match (left, right) {
-            (Object::Integer(l), Object::Integer(r)) => Ok(Object::Boolean(l < r)),
-            (l, r) => Err(format!("Cannot compare equality between {} and {}", l, r)),
-        }
-    }
-
-    fn eval_greater_infix(&self, left: Object, right: Object) -> Result<Object> {
-        match (left, right) {
-            (Object::Integer(l), Object::Integer(r)) => Ok(Object::Boolean(l > r)),
-            (l, r) => Err(format!("Cannot compare equality between {} and {}", l, r)),
-        }
-    }
-
-    fn eval_equal_infix(&self, left: Object, right: Object) -> Result<Object> {
-        match (left, right) {
-            (Object::Integer(l), Object::Integer(r)) => Ok(Object::Boolean(l == r)),
-            (Object::Boolean(l), Object::Boolean(r)) => Ok(Object::Boolean(l == r)),
-            (l, r) => Err(format!("Cannot compare equality between {} and {}", l, r)),
-        }
-    }
-
-    fn eval_not_equal_infix(&self, left: Object, right: Object) -> Result<Object> {
-        match (left, right) {
-            (Object::Integer(l), Object::Integer(r)) => Ok(Object::Boolean(l != r)),
-            (Object::Boolean(l), Object::Boolean(r)) => Ok(Object::Boolean(l != r)),
-            (l, r) => Err(format!("Cannot compare equality between {} and {}", l, r)),
         }
     }
 }
